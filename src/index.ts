c879b0e4485a0ad5b7725ead8818a9df17c0f8b5
Timoyoungster <timo.proemer04@gmail.com>
2026-03-08 14:02:53 +0100

    fix: export WeekdayTimeslotRangeInput

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { generateTimeslots } from './generate-timeslots.js';
 export { generateDailyTimeslots } from './generate-daily-timeslots.js';
-export type { DateTimeslotRangeInput, DailyTimeslotConfig } from './generate-daily-timeslots.js';
+export type { WeekdayTimeslotRangeInput, DateTimeslotRangeInput, DailyTimeslotConfig } from './generate-daily-timeslots.js';
 export { createTimeslot, overlaps, contains, mergeSlots, findGaps, timeslotToJSON, timeslotFromJSON } from './timeslot.js';
 export type { TimeslotJSON } from './timeslot.js';
 
